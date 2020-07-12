e022a393e41a489cde57b9483e9a3cc030bb6cf7
bdon-htb <46268307+bdon-htb@users.noreply.github.com>
2020-07-12 18:08:11 -0400

    Did more kivy tests.

---
+++ tests/bdon/python/kivy_test11/kivy_test11.py
@@ -0,0 +1,45 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.settings import SettingsWithSidebar
+
+from settings_json import settings_json
+
+Builder.load_string('''
+<Interface>
+    orientation: 'vertical'
+    Button:
+        text: 'open the settings!'
+        font_size: 150
+        on_release: app.open_settings()
+''')
+
+class Interface(BoxLayout):
+    pass
+
+class SettingsApp(App):
+    def build(self):
+        self.settings_cls = SettingsWithSidebar
+        # self.use_kivy_settings = False
+        return Interface()
+
+    def build_config(self, config):
+        config.setdefaults('example', {
+        'boolexample': True,
+        'numericexample': 10,
+        'optionsexample': 'option2',
+        'stringexample': 'some_string',
+        'pathexample': '/some/path'
+        })
+
+    def build_settings(self, settings):
+        settings.add_json_panel('Panel Name',
+            self.config,
+            data=settings_json)
+
+    def on_config_change(self, config, section, key, value):
+        # settings = self.config.get('example', 'boolexample')
+        print(config + " " section + " " key + " " value)
+
+SettingsApp().run()

bdbe83e14446f714f6a3d6d152401b458afd3959
bdon-htb <46268307+bdon-htb@users.noreply.github.com>
2020-07-15 17:12:56 -0400

    Start working on new level editor

---
+++ tools/levelEditor/main.js
@@ -0,0 +1,10 @@
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext("2d");
+ctx.fillStyle = "#B8D5EE";
+ctx.fillRect(0, 0, 800, 600)
+
+let canvas2 = document.getElementById('canvas2');
+let ctx2 = canvas2.getContext("2d");
+ctx2.fillStyle = "red";
+ctx2.fillRect(0, 0, 400, 300)
+console.log("Hello world!");

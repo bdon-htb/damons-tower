6f80c94fd2a6f57b7083ec68080c55c4d2720d38
bdon-htb <46268307+bdon-htb@users.noreply.github.com>
2020-12-12 23:55:26 -0500

    Lay groundwork for level_editor

---
+++ tools/level_editor/Code/cfg.py
@@ -0,0 +1,11 @@
+# ========================================================
+# cfg.py contains file paths and general app information.
+# ========================================================
+
+import os
+
+__version__ = '0.0.0'
+__name__ = 'js-roguelite Level Editor'
+
+main_dir = os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..'))
+code_dir = os.path.abspath(os.path.join(main_dir, 'Code'))
--- tools/level_editor/Code/widgets.py
+++ tools/level_editor/Code/widgets.py
@@ -1,4 +1,12 @@
+# =======================================================
+# widget.py contains all GUI-related methods and classes.
+# =======================================================
+
+# PyQt imports
 from PyQt5.QtWidgets import QMainWindow
+
+# Custom imports
+from . import cfg
 
 class MainWindow(QMainWindow):
     def __init__(self, parent):
